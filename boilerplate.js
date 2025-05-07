b5646cf1d78d425613f03664f8de2dca6e049a2e
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2025-05-07 14:52:29 +0100

    Add files via upload

---
+++ boilerplate.js
@@ -0,0 +1,17 @@
+const fs = require('node:fs/promises');
+const fileName = process.argv[] || 'Project';
+
+async function createFile() {
+	try {
+		await fs.mkdir(`${fileName}`);
+		await fs.writeFile(`${fileName}/index.html`);
+	} catch (error){
+		if (error.code === 'EEXIST'){
+			console.log('Directory already exists');
+		} else {
+			console.error('Error:', error);
+		}
+	}
+}
+
+createFile();
